aadff1a1b7542ec7a607821aede3a55076b37576
Dang Tuan Binh <tuanbinh.hehe@gmail.com>
2023-06-05 22:20:23 +0700

    Routing and Setup folder

--- Twitter/src/index.ts
+++ Twitter/src/index.ts
@@ -1,3 +1,17 @@
-const fullName: string = 'Đặng Tuấn Bình 12345678'
-const age: number = 20
-console.log(fullName)
+import express from 'express'
+import usersRouter from './routes/users.routes'
+const app = express()
+const port = 4000
+app.use(express.json())
+const router = express.Router()
+
+router.post('/', (req, res) => {
+  console.log('hello world')
+})
+
+app.use('/users', usersRouter)
+
+module.exports = router
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
