1a40d56aa245270e9b8f26b82ad0c9cf6882a89f
Dang Tuan Binh <tuanbinh.hehe@gmail.com>
2023-07-01 15:13:21 +0700

    Refresh-token

--- Twitter/src/services/user.services.ts
+++ Twitter/src/services/user.services.ts
@@ -392,6 +392,29 @@ class UserServices {
       return { ...data, newUser: 1, verify: UserVerifyStatus.Unverified }
     }
   }
+  async refreshToken({
+    user_id,
+    verify,
+    refresh_token
+  }: {
+    user_id: string
+    verify: UserVerifyStatus
+    refresh_token: string
+  }) {
+    const [new_access_token, new_refresh_token] = await Promise.all([
+      this.signAccessToken({ user_id, verify }),
+      this.signRefreshToken({ user_id, verify }),
+      databaseServices.refreshToken.deleteOne({ token: refresh_token })
+    ])
+    await databaseServices.refreshToken.insertOne(
+      new RefreshToken({ user_id: new ObjectId(user_id), token: new_refresh_token })
+    )
+
+    return {
+      access_token: new_access_token,
+      refresh_token: new_refresh_token
+    }
+  }
 }
 const userServices = new UserServices()
 export default userServices
